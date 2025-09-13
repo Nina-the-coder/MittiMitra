067232b3436739a3a8e182ed450687c838207bc5
Deepanshu <n09456791@gmail.com>
2025-09-13 09:13:57 +0530

    made the frontEnd folder structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="h-20 bg-red-500 flex justify-center items-center">hello
+      </div>
+    </>
+  )
+}
+
+export default App
